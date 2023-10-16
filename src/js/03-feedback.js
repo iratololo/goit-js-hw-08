83258229a7eb8ba1c720ea79f88e36a26d6ed235
iratololo <iratololo1991@gmail.com>
2023-10-16 12:05:42 +0300

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,50 @@
+const throttle = require('lodash.throttle');
+
+
+const selectors = {
+    form: document.querySelector(".feedback-form"),
+}
+
+
+selectors.form.addEventListener("input", throttle(hendlerFormСhange, 500));
+
+window.addEventListener("load", hendlerStorage);
+
+selectors.form.addEventListener("submit", hendlerFormSubmit);
+
+
+const settings = {};
+
+function hendlerFormСhange(evt) {
+    settings[evt.target.name] = evt.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(settings));
+}
+
+
+
+function hendlerFormSubmit(event) {
+    event.preventDefault();
+    const { email, message } = event.currentTarget.elements;
+    settings.email = email.value;
+    settings.message = message.value;
+    console.log(settings);
+    event.currentTarget.reset();
+    localStorage.clear();
+}
+
+
+function hendlerStorage() {
+    if (localStorage.length) {
+    const savedSettings = localStorage.getItem("feedback-form-state");
+    const parsedSettings = JSON.parse(savedSettings);
+    selectors.form.elements.email.value = parsedSettings.email || "";
+    selectors.form.elements.message.value = parsedSettings.message || "";
+}
+}
+
+
+
+
+
+
 
